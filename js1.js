220a64bbfca7771fca4504d325196d9b92e42f75
parassaini <31025565+sainiparas@users.noreply.github.com>
2018-02-08 09:11:21 -0800

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,10 @@
+function pict()
+{
+	alert("No any image exist right now. Please try after sometime.");
+}
+
+
+function vid()
+{
+	alert("No any video exist right now. Please try after sometime.");
+}
